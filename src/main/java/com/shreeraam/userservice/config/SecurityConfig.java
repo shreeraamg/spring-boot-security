396c9b499ef02014bb697da3a4b56962729fc9a4
Shreeraam G <shreeram0312@gmail.com>
2024-06-14 07:48:20 +0530

    Reduce hashing rounds

--- src/main/java/com/shreeraam/userservice/config/SecurityConfig.java
+++ src/main/java/com/shreeraam/userservice/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(16);
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
